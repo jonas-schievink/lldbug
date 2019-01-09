d129dcc2d9d7e2111c19d83f3eec931d3a1095b8
Jonas Schievink <jonasschievink@gmail.com>
2019-01-09 18:04:05 +0100

    improve the test script

--- bad.sh
+++ bad.sh
@@ -6,7 +6,13 @@ function bail
 	exit 2
 }
 
+export RUSTFLAGS=-Ccodegen-units=1
+export CARGO_INCREMENTAL=0
 cargo build || bail "build failed"
-readelf --wide --debug-dump=rawline target/debug/ta-client > /dev/null 2>&1 || bail "still broken"
+output=$(readelf --wide --debug-dump=rawline target/debug/ta-client 2>&1 > /dev/null)
 
-echo "appears to be good"
+echo "$output"
+echo "$output" | grep "the section is too small" > /dev/null || bail "!!! appears to be FIXED !!!"
+
+echo "still broken - good"
+exit 0
