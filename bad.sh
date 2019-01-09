b1d51e7a1591b1386d51ed702204edb161d85b72
Jonas Schievink <jonasschievink@gmail.com>
2019-01-09 18:49:33 +0100

    bad.sh: allow passing cargo parameters

--- bad.sh
+++ bad.sh
@@ -8,7 +8,7 @@ function bail
 
 export RUSTFLAGS=-Ccodegen-units=1
 export CARGO_INCREMENTAL=0
-cargo build || bail "build failed"
+cargo build $@ || bail "build failed"
 output=$(readelf --wide --debug-dump=rawline target/debug/ta-client 2>&1 > /dev/null)
 
 echo "$output"
